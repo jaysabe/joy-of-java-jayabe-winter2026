ab2449595a21acf2e2d346e9a1ca1854c74fb747
jayabe <jayabe@pdx.edu>
2026-01-22 15:03:19 -0800

    add first proj inits

---
+++ phonebill/src/main/java/edu/pdx/cs/joy/jayabe/Project1.java
@@ -0,0 +1,23 @@
+package edu.pdx.cs.joy.jayabe;
+
+import com.google.common.annotations.VisibleForTesting;
+
+/**
+ * The main class for the Phone Bill Project
+ */
+public class Project1 {
+
+  @VisibleForTesting
+  static boolean isValidDateAndTime(String dateAndTime) {
+    return true;
+  }
+
+  public static void main(String[] args) {
+    PhoneCall call = new PhoneCall();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    System.err.println("Missing phone bill information");
+    for (String arg : args) {
+      System.out.println(arg);
+    }
+  }
+
+}
