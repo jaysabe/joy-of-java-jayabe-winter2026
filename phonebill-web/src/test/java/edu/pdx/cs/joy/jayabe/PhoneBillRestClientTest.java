a0cf0a3cecc7d1210269ab3d13881b736eeb687f
jaysabe <jsabegglen96@gmail.com>
2026-03-01 22:40:12 -0800

    add phonebill-web

---
+++ phonebill-web/src/test/java/edu/pdx/cs/joy/jayabe/PhoneBillRestClientTest.java
@@ -0,0 +1,38 @@
+package edu.pdx.cs.joy.jayabe;
+
+import edu.pdx.cs.joy.ParserException;
+import edu.pdx.cs.joy.web.HttpRequestHelper;
+import edu.pdx.cs.joy.web.HttpRequestHelper.Response;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PhoneBillRestClientTest {
+
+  @Test
+  void getAllDictionaryEntriesPerformsHttpGetWithNoParameters() throws ParserException, IOException {
+    Map<String, String> dictionary = Map.of("One", "1", "Two", "2");
+
+    HttpRequestHelper http = mock(HttpRequestHelper.class);
+    when(http.get(eq(Map.of()))).thenReturn(dictionaryAsText(dictionary));
+
+    PhoneBillRestClient client = new PhoneBillRestClient(http);
+
+    assertThat(client.getAllDictionaryEntries(), equalTo(dictionary));
+  }
+
+  private Response dictionaryAsText(Map<String, String> dictionary) {
+    StringWriter writer = new StringWriter();
+    new TextDumper(writer).dump(dictionary);
+
+    return new Response(writer.toString());
+  }
+}
